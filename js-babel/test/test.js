e9da2c32924c58c7ef663c9f890015bbde5f8b8d
Phil Cockfield <phil@cockfield.net>
2016-02-03 10:04:26 +1300

    Tests for class "property initializers".
    Ref: https://github.com/jeffmo/es-class-fields-and-static-properties

--- js-babel/test/test.js
+++ js-babel/test/test.js
@@ -2,10 +2,31 @@ import { expect } from 'chai';
 
 
 describe('babel', () => {
-  it('has a class property', () => {
+  it('supports static class properties', () => {
     class Foo {
       static myStatic = 123;
     };
     expect(Foo.myStatic).to.equal(123);
+  });
+
+
+  it('supports class "property initializer"', () => {
+    // See:
+    //    ES Class Fields & Static Properties
+    //    https://github.com/jeffmo/es-class-fields-and-static-properties
+    class Foo {
+      number = 123;
+      func = () => "my-foo";
+    };
+    const foo1 = new Foo();
+    expect(Foo.number).to.equal(undefined);
+    expect(Foo.func).to.equal(undefined);
+    expect(foo1.number).to.equal(123);
+    expect(foo1.func()).to.equal("my-foo");
+
+    foo1.number = 555;
+    const foo2 = new Foo();
+    expect(foo1.number).to.equal(555);
+    expect(foo2.number).to.equal(123);
   });
 });
